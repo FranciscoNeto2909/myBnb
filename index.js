b7a78f9dafe2a3cb0efb47cc141f64a59ba44917
Francisco Neto <wantedshark@gmail.com>
2022-08-17 12:31:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const cors = require("cors")
+const port = process.env.PORT || "3000"
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.listen(port, console.log("executando..."))
+
+app.get("/", (req, res)=>{
+    return res.send(db)
+})
+app.post("/newschedule", (req, res)=>{
+    const schedule = req.body
+    if (schedule.requisitioner !== "") {
+        db.schedules.push(schedule)
+        res.send(schedule)
+    } else {
+        console.error()
+    }
+})
+app.delete("/delschedule/:id",(req, res)=>{
+    const scheduleId = req.params.id
+    db.schedules = db.schedules.filter(scdl => scdl.id.toString() !== scheduleId.toString())
+})
